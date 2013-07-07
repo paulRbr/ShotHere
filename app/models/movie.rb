b8fc40bba13f0ccc31dbb1bec8ed7795a6e68fed
Paul Bonaud <paul@bonaud.fr>
2013-07-08 01:12:41 +0200

    Retrieve some info from IMDB

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,18 +1,20 @@
 require 'net/http'
 require 'uri'
+require 'json'
 
 class Movie < ActiveRecord::Base
   attr_accessible :imdb_id, :title
   
-  before_validation :imdb_id_exists?
   validates_presence_of :imdb_id
+  validates_uniqueness_of :imdb_id
+  before_validation :populate_from_imdb, :only => [:imdb_id]
 
-  private
-  def imdb_id_exists?
-    uri = URI.parse("http://mymovieapi.com/?id=tt#{@imdb_id}&type=json")
+  def populate_from_imdb
+    uri = URI.parse("http://mymovieapi.com/?id=tt#{self.imdb_id}&type=json")
     response = Net::HTTP.get_response(uri)
     Rails.logger.debug response.body
-    true
+    data = JSON.parse response.body
+    self.title = data["title"]
   end
 
 end

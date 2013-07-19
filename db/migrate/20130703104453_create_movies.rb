b2e30d587508b73b78a34d4ecb4ef320d97df4e3
Paul Bonaud <paul@bonaud.fr>
2013-07-19 10:29:53 +0200

    Possible to add the imdb url directly

--- db/migrate/20130703104453_create_movies.rb
+++ db/migrate/20130703104453_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
-      t.integer :imdb_id
+      t.string :imdb_id
 
       t.timestamps
     end

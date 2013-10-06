f35c3435d740ccbba357eecdd4efe6c9c39bdb41
Paul B <paul@bonaud.fr>
2013-10-07 00:27:01 +0200

    Regenerate Private keys for devise and make them ENV variables out of the public repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Shothere::Application.config.secret_token = '42554de672f9219d93aee1c5d1b3f527520e8bd5a648dfd659a91833585f00213d9f2c16431bd564d238121a6b87ce53ace91b4dd11e6c71199ca496eb994b7e'
+Shothere::Application.config.secret_token = ENV['DEVISE_KEY']

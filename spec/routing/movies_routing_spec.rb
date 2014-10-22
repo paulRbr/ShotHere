38793f8a71fa9639fc3992a99e69d58d1997ec56
Paul B <paul@bonaud.fr>
2014-10-22 22:52:29 +0200

    Revert "chore(gems): major update of old gems. TODO: some tests to fix"
    This reverts commit 82c50eda8f3d786e57c8a946f8844ebe37848d5f.
    
    Conflicts:
    	Gemfile.lock

--- spec/routing/movies_routing_spec.rb
+++ spec/routing/movies_routing_spec.rb
@@ -1,34 +1,34 @@
 require "spec_helper"
 
 describe MoviesController do
-  describe "routing", type: :routing do
+  describe "routing" do
 
     it "routes to #index" do
-      expect(get: "/movies").to route_to("movies#index")
+      get("/movies").should route_to("movies#index")
     end
 
     it "routes to #new" do
-      expect(get: "/movies/new").to route_to("movies#new")
+      get("/movies/new").should route_to("movies#new")
     end
 
     it "routes to #show" do
-      expect(get: "/movies/1").to route_to("movies#show", :id => "1")
+      get("/movies/1").should route_to("movies#show", :id => "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/movies/1/edit").to route_to("movies#edit", :id => "1")
+      get("/movies/1/edit").should route_to("movies#edit", :id => "1")
     end
 
     it "routes to #create" do
-      expect(post: "/movies").to route_to("movies#create")
+      post("/movies").should route_to("movies#create")
     end
 
     it "routes to #update" do
-      expect(put: "/movies/1").to route_to("movies#update", :id => "1")
+      put("/movies/1").should route_to("movies#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/movies/1").to route_to("movies#destroy", :id => "1")
+      delete("/movies/1").should route_to("movies#destroy", :id => "1")
     end
 
   end
